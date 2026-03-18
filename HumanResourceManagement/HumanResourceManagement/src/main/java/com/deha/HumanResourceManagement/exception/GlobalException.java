1085780f265d3d5fe4c8658a68f8eb7850c90f67
trung2604 <dodinhtrungthptyv@gmail.com>
2026-03-18 15:23:54 +0700

    feat:crud get all employee and get employee by id

--- HumanResourceManagement/HumanResourceManagement/src/main/java/com/deha/HumanResourceManagement/exception/GlobalException.java
+++
@@ -1,4 +0,0 @@
-package com.deha.HumanResourceManagement.exception;
-
-public class GlobalException {
-}
--- HumanResourceManagement/HumanResourceManagement/src/main/java/com/deha/HumanResourceManagement/repository/EmployeeRepository.java
+++ HumanResourceManagement/HumanResourceManagement/src/main/java/com/deha/HumanResourceManagement/repository/EmployeeRepository.java
@@ -1,4 +1,12 @@
 package com.deha.HumanResourceManagement.repository;
 
-public class EmployeeRepository {
+
+import com.deha.HumanResourceManagement.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+    boolean existsByEmail(String email);
 }
